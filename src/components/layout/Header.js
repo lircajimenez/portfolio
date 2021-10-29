081e0d6118c2c738e5a37f79b2940cf79731a016
Lirca Jimenez <lirca.je@gmail.com>
2021-10-29 16:04:00 -0400

    draft navbar

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,43 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const Header = () => {
-  return <Wrapper>HEADER</Wrapper>;
+  return (
+    <Wrapper>
+      <Nav>
+        <ul>
+          <li>
+            <NavLink to="#projects">PROJECTS</NavLink>
+          </li>
+          <li>
+            <NavLink to="#about">ABOUT</NavLink>
+          </li>
+          <li>
+            <NavLink to="#contact">CONTACT</NavLink>
+          </li>
+        </ul>
+      </Nav>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.header`
-  border: 10px red solid;
+  width: 100%;
+  height: 6vh;
+  background: #272829;
+  position: fixed;
+  display: flex;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  /* justify-content: space-between; */
+
+  ul {
+    display: flex;
+    justify-content: space-between;
+  }
 `;
 
 export default Header;
